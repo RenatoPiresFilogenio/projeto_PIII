c41d17ee8d4780eaf6f4d7b17317e4a141a18eff
Your Name <you@example.com>
2025-09-29 19:23:21 -0300

    Commit inicial - histórico limpo

---
+++ backend/DB/database.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$port = "5432";
+$dbname = "SolarSmart";
+$usuario = "postgres";
+$senha = "=====";
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $usuario, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão bem-sucedida!";
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
