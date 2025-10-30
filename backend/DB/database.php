11f443f270bf6654e20043f79ee5d5401fddd049
Your Name <you@example.com>
2025-10-30 17:09:00 -0300

    configurando banco_web

--- backend/DB/database.php
+++ backend/DB/database.php
@@ -1,15 +1,35 @@
 <?php
-$host = "localhost";      // Servidor local (XAMPP)
-$port = "3306";           // Porta padrão do MySQL
-$dbname = "solarsmart"; // Nome do banco criado no phpMyAdmin
-$usuario = "root";        // Usuário padrão do XAMPP
-$senha = "";              // Senha padrão do XAMPP é vazia
+
+require __DIR__ . '/../../vendor/autoload.php';
+
 
 try {
-    // Conexão com MySQL (XAMPP)
-    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $usuario, $senha);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    $e->getMessage();
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+    $dotenv->load();
+} catch (Exception $e) {
+    die("Erro ao carregar o arquivo .env: " . $e->getMessage());
 }
-?>
+
+$host     = $_ENV['DB_HOST'];
+$port     = $_ENV['DB_PORT'];
+$dbname   = $_ENV['DB_NAME'];
+$usuario  = $_ENV['DB_USER'];
+$senha    = $_ENV['DB_PASS']; 
+$endpoint = $_ENV['DB_ENDPOINT'];
+$sslmode  = "require";
+
+try {
+  
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=$sslmode;options=endpoint=$endpoint";
+
+    $pdo = new PDO($dsn, $usuario, $senha);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  
+} catch (PDOException $e) {
+   
+    die("Erro ao conectar com o banco de dados: " . $e->getMessage());
+}
+
+
